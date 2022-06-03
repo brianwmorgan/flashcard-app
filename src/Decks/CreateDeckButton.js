077d526b5d1be8f8f8020a1b018f9be4515d881f
Brian Morgan <brianwmorgan317@gmail.com>
2022-06-03 11:37:55 -0400

    minor formatting edits

--- src/Decks/CreateDeckButton.js
+++ src/Decks/CreateDeckButton.js
@@ -2,21 +2,19 @@ import React from "react";
 import { useHistory } from "react-router-dom";
 
 function CreateDeckButton() {
-    
-    const history = useHistory();
-    
-    return (
-        <div>
-            <button
-                type="button"
-                className="btn btn-secondary my-2"
-                onClick={ () => history.push("/decks/new") }
-            >
-                Create Deck
-            </button>
-        </div>
-        
-    )
-};
+  const history = useHistory();
+
+  return (
+    <div>
+      <button
+        type="button"
+        className="btn btn-secondary my-2"
+        onClick={() => history.push("/decks/new")}
+      >
+        Create Deck
+      </button>
+    </div>
+  );
+}
 
 export default CreateDeckButton;
